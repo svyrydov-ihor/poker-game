efaa16b48e3f3e258ee719de3222d6f8deeeaaa6
svyrydov-ihor <148194827+svyrydov-ihor@users.noreply.github.com>
2025-05-02 19:20:48 +0300

    State pattern for Pre-flop/Flop flow and Template method for betting round implemented

--- app/game/game.py
+++ app/game/game.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from typing import List
 
 from app.game.game_phases import *
+from app.game.game_states import PreFlopState, AbsGameState
 from app.game.models import Player, Card
 from app.game.table import Table
 
@@ -38,6 +39,10 @@ class Game:
         self.sb_pos = -1
         self.bb_pos = -1
         self.is_game_started = False
+        self.game_state = None
+
+    def set_game_state(self, game_state: AbsGameState):
+        self.game_state = game_state
 
     async def start_game(self):
         self.is_game_started = True
@@ -51,158 +56,6 @@ class Game:
             prev_dealer=self.players[self.prev_dealer_pos],
             curr_dealer=self.players[self.curr_dealer_pos],
             ordered_player_ids=ordered_ids))
-        await self.pre_flop()
-        await self.pre_flop_betting()
-        await self.flop()
 
-    async def pre_flop(self):
-        for player in self.players:
-            player.pocket_cards = self.table.get_cards(2)
-            await self.game_handler.send_personal(GamePhase.POCKET_CARDS, player.id, PocketCardsArgs(
-                pocket_cards=player.get_poket_cards_dict()))
-
-        #small blind
-        self.sb_pos = (self.curr_dealer_pos + 1) % len(self.players)
-        self.players[self.sb_pos].balance -= self.sb_amount
-        self.players[self.sb_pos].bet = self.sb_amount
-        self.pot += self.sb_amount
-        await self.game_handler.broadcast(GamePhase.PRE_FLOP_SB, PreFlopSBArgs(
-            sb_amount=self.sb_amount,
-            player=self.players[self.sb_pos]))
-        await self.game_handler.broadcast(GamePhase.POT, PotArgs(pot=self.pot))
-
-        #big blind
-        self.bb_pos = (self.sb_pos + 1) % len(self.players)
-        self.players[self.bb_pos].balance -= self.bb_amount
-        self.players[self.bb_pos].bet = self.bb_amount
-        self.pot += self.bb_amount
-        await self.game_handler.broadcast(GamePhase.PRE_FLOP_BB, PreFlopBBArgs(
-            bb_amount=self.bb_amount,
-            player=self.players[self.bb_pos]))
-        await self.game_handler.broadcast(GamePhase.POT, PotArgs(pot=self.pot))
-
-    async def pre_flop_betting(self):
-        curr_player_pos = (self.bb_pos + 1) % len(self.players)
-        prev_player_pos = self.bb_pos
-        bb_next_pos = (self.bb_pos + 1) % len(self.players)
-
-        turn_options = [PlayerChoice.CALL, PlayerChoice.FOLD, PlayerChoice.RAISE]
-        while True:
-            if curr_player_pos == self.bb_pos:
-                turn_options = [PlayerChoice.CHECK, PlayerChoice.FOLD, PlayerChoice.RAISE]
-
-            processed_turn = await self.process_turn(curr_player_pos, prev_player_pos, self.min_raise, turn_options)
-            prev_player_pos = curr_player_pos
-
-            if processed_turn.is_raised:
-                await self.betting(curr_player_pos, processed_turn.curr_raise)
-                break
-
-            prev_player_pos = curr_player_pos
-            curr_player_pos = (curr_player_pos + 1) % len(self.players)
-
-            if curr_player_pos == bb_next_pos:
-                break
-        await self.game_handler.broadcast(GamePhase.TURN_HIGHLIGHT, TurnHighlightArgs(
-            prev_player=self.players[prev_player_pos]
-        ))
-
-    async def betting(self, bet_player_pos, raise_amount):
-        curr_player_pos = (bet_player_pos + 1) % len(self.players)
-        prev_player_pos = bet_player_pos
-        while True:
-            turn_options = [PlayerChoice.CALL, PlayerChoice.FOLD, PlayerChoice.RAISE]
-
-            processed_turn = await self.process_turn(curr_player_pos, prev_player_pos, raise_amount, turn_options)
-
-            prev_player_pos = curr_player_pos
-
-            if processed_turn.is_raised:
-                await self.betting(curr_player_pos, processed_turn.curr_raise)
-                break
-
-            curr_player_pos = (curr_player_pos + 1) % len(self.players)
-
-            if curr_player_pos == bet_player_pos:
-                break
-        await self.game_handler.broadcast(GamePhase.TURN_HIGHLIGHT, TurnHighlightArgs(
-            prev_player=self.players[prev_player_pos]
-        ))
-
-    async def process_turn(self, curr_player_pos, prev_player_pos, prev_raise, options: List[PlayerChoice])->ProcessedTurn:
-        await self.game_handler.broadcast(GamePhase.TURN_HIGHLIGHT, TurnHighlightArgs(
-                prev_player=self.players[prev_player_pos],
-                curr_player=self.players[curr_player_pos]))
-
-        p_choice = await self.game_handler.turn(self.players[curr_player_pos],
-                                                TurnRequestArgs(
-                                                    player_bet=self.players[curr_player_pos].bet,
-                                                    prev_bet=self.players[prev_player_pos].bet,
-                                                    prev_raise=prev_raise,
-                                                    options=options
-                                                ))
-        to_call = self.players[prev_player_pos].bet - self.players[curr_player_pos].bet
-
-        is_raised = False
-        curr_raise = prev_raise
-
-        match p_choice.choice:
-            case PlayerChoice.CALL:
-                self.players[curr_player_pos].bet += p_choice.amount
-                self.players[curr_player_pos].balance -= p_choice.amount
-                self.pot += p_choice.amount
-            case PlayerChoice.RAISE:
-                self.players[curr_player_pos].bet += to_call + p_choice.amount
-                self.players[curr_player_pos].balance -= to_call + p_choice.amount
-                self.pot += to_call + p_choice.amount
-                curr_raise = p_choice.amount
-                is_raised = True
-            case PlayerChoice.FOLD:
-                self.folded.append(self.players[curr_player_pos])
-            case PlayerChoice.CHECK:
-                pass
-
-        await self.game_handler.broadcast(GamePhase.TURN_RESULT, TurnResultArgs(
-            player=self.players[curr_player_pos],
-            choice=p_choice.choice,
-            amount=self.players[curr_player_pos].bet,
-        ))
-        await self.game_handler.broadcast(GamePhase.POT, PotArgs(pot=self.pot))
-
-        return ProcessedTurn(
-            is_raised=is_raised,
-            curr_bet=self.players[curr_player_pos].bet,
-            curr_raise=curr_raise
-        )
-
-    async def flop(self):
-        self.table.community_cards += self.table.get_cards(3)
-        await self.game_handler.broadcast(GamePhase.COMMUNITY_CARDS, CommunityCardsArgs(
-            cards=self.table.community_cards
-        ))
-
-        for player in self.players: player.bet = 0
-
-        prev_player_pos = self.curr_dealer_pos
-        curr_player_pos = (self.curr_dealer_pos + 1) % len(self.players)
-        next_to_dealer = curr_player_pos
-
-        while True:
-            turn_options = [PlayerChoice.CHECK, PlayerChoice.FOLD, PlayerChoice.RAISE]
-
-            processed_turn = await self.process_turn(curr_player_pos, prev_player_pos, self.min_raise, turn_options)
-
-            prev_player_pos = curr_player_pos
-
-            if processed_turn.is_raised:
-                await self.betting(curr_player_pos, processed_turn.curr_raise)
-                break
-
-            curr_player_pos = (curr_player_pos + 1) % len(self.players)
-
-            if curr_player_pos == next_to_dealer:
-                break
-        await self.game_handler.broadcast(GamePhase.TURN_HIGHLIGHT, TurnHighlightArgs(
-            prev_player=self.players[prev_player_pos]
-        ))
-
+        self.set_game_state(PreFlopState(self))
+        await self.game_state.start_flow()
---
+++ app/game/game_states.py
@@ -0,0 +1,203 @@
+from abc import ABC, abstractmethod
+
+from typing_extensions import override
+
+from app.game.game_phases import *
+from typing import TYPE_CHECKING, List, Set
+
+if TYPE_CHECKING:
+    from app.game.game import Game
+
+
+class AbsGameState(ABC):
+    def __init__(self, game: 'Game'):
+        self.game = game
+
+    @abstractmethod
+    async def start_flow(self):
+        pass
+
+    async def run_betting_round(self):
+        """
+        Template method for betting round
+        """
+        await self._before_betting_round_action() # hook
+        turn_options = self._get_init_turn_options() # hook
+        aggressor_pos = self._get_init_aggressor_pos() # hook
+        curr_player_pos = self._get_starting_pos() #hook
+        prev_player_pos = (curr_player_pos - 1 + len(self.game.players)) % len(self.game.players)
+        min_raise = self._get_min_raise() # hook
+        is_raised = False
+
+        while True:
+            if not is_raised: turn_options = self._change_turn_options(curr_player_pos, turn_options)
+            processed_turn = await self._process_turn(
+                curr_player_pos=curr_player_pos,
+                prev_player_pos=prev_player_pos,
+                prev_raise=min_raise,
+                options=turn_options)
+
+            if processed_turn.is_raised:
+                aggressor_pos = curr_player_pos
+                min_raise = processed_turn.curr_raise
+                turn_options = [PlayerChoice.CALL, PlayerChoice.FOLD, PlayerChoice.RAISE]
+                is_raised = True
+
+            prev_player_pos = curr_player_pos
+            curr_player_pos = (curr_player_pos + 1) % len(self.game.players)
+
+            if curr_player_pos == aggressor_pos:
+                break
+        await self.game.game_handler.broadcast(GamePhase.TURN_HIGHLIGHT, TurnHighlightArgs(
+            prev_player=self.game.players[prev_player_pos]))
+
+    @abstractmethod
+    async def _before_betting_round_action(self):
+        pass
+
+    @abstractmethod
+    def _get_init_turn_options(self):
+        pass
+
+    @abstractmethod
+    def _get_init_aggressor_pos(self):
+        pass
+
+    @abstractmethod
+    def _get_starting_pos(self):
+        pass
+
+    def _get_min_raise(self):
+        return self.game.min_raise
+
+    def _change_turn_options(self, curr_player_pos, turn_options: List[PlayerChoice])->List[PlayerChoice]:
+        """
+        overridden in PreFlopState
+        """
+        return turn_options
+
+    async def _deal_community_cards(self, number: int):
+        self.game.table.community_cards += self.game.table.get_cards(number)
+        await self.game.game_handler.broadcast(GamePhase.COMMUNITY_CARDS, CommunityCardsArgs(
+            cards=self.game.table.community_cards
+        ))
+
+    async def _process_turn(self, curr_player_pos, prev_player_pos, prev_raise, options: List[PlayerChoice])->ProcessedTurn:
+        await self.game.game_handler.broadcast(GamePhase.TURN_HIGHLIGHT, TurnHighlightArgs(
+                prev_player=self.game.players[prev_player_pos],
+                curr_player=self.game.players[curr_player_pos]))
+
+        p_choice = await self.game.game_handler.turn(self.game.players[curr_player_pos],
+                                                TurnRequestArgs(
+                                                    player_bet=self.game.players[curr_player_pos].bet,
+                                                    prev_bet=self.game.players[prev_player_pos].bet,
+                                                    prev_raise=prev_raise,
+                                                    options=options
+                                                ))
+        to_call = self.game.players[prev_player_pos].bet - self.game.players[curr_player_pos].bet
+
+        is_raised = False
+        curr_raise = prev_raise
+
+        match p_choice.choice:
+            case PlayerChoice.CALL:
+                self.game.players[curr_player_pos].bet += p_choice.amount
+                self.game.players[curr_player_pos].balance -= p_choice.amount
+                self.game.pot += p_choice.amount
+            case PlayerChoice.RAISE:
+                self.game.players[curr_player_pos].bet += to_call + p_choice.amount
+                self.game.players[curr_player_pos].balance -= to_call + p_choice.amount
+                self.game.pot += to_call + p_choice.amount
+                curr_raise = p_choice.amount
+                is_raised = True
+            case PlayerChoice.FOLD:
+                self.game.folded.append(self.game.players[curr_player_pos])
+            case PlayerChoice.CHECK:
+                pass
+
+        await self.game.game_handler.broadcast(GamePhase.TURN_RESULT, TurnResultArgs(
+            player=self.game.players[curr_player_pos],
+            choice=p_choice.choice,
+            amount=self.game.players[curr_player_pos].bet,
+        ))
+        await self.game.game_handler.broadcast(GamePhase.POT, PotArgs(pot=self.game.pot))
+
+        return ProcessedTurn(
+            is_raised=is_raised,
+            curr_bet=self.game.players[curr_player_pos].bet,
+            curr_raise=curr_raise
+        )
+
+class PreFlopState(AbsGameState):
+    def _get_init_turn_options(self):
+        return [PlayerChoice.CALL, PlayerChoice.FOLD, PlayerChoice.RAISE]
+
+    def _get_init_aggressor_pos(self):
+        return (self.game.bb_pos + 1) % len(self.game.players)
+
+    @override
+    def _get_starting_pos(self):
+        return (self.game.bb_pos + 1) % len(self.game.players)
+
+    def _get_min_raise(self):
+        return self.game.min_raise
+
+    def _change_turn_options(self, curr_player_pos, turn_options: List[PlayerChoice])->List[PlayerChoice]:
+        if curr_player_pos == self.game.bb_pos: return [PlayerChoice.CHECK, PlayerChoice.FOLD, PlayerChoice.RAISE]
+        else: return turn_options
+
+    async def start_flow(self):
+        await self.deal_player_cards()
+        await self.run_betting_round()
+        next_state = FlopState(self.game)
+        self.game.set_game_state(next_state)
+        await next_state.start_flow()
+
+    async def deal_player_cards(self):
+        for player in self.game.players:
+            player.pocket_cards = self.game.table.get_cards(2)
+            await self.game.game_handler.send_personal(GamePhase.POCKET_CARDS, player.id, PocketCardsArgs(
+                pocket_cards=player.get_poket_cards_dict()))
+
+    async def _before_betting_round_action(self):
+        # small blind
+        self.game.sb_pos = (self.game.curr_dealer_pos + 1) % len(self.game.players)
+        self.game.players[self.game.sb_pos].balance -= self.game.sb_amount
+        self.game.players[self.game.sb_pos].bet = self.game.sb_amount
+        self.game.pot += self.game.sb_amount
+        await self.game.game_handler.broadcast(GamePhase.PRE_FLOP_SB, PreFlopSBArgs(
+            sb_amount=self.game.sb_amount,
+            player=self.game.players[self.game.sb_pos]))
+        await self.game.game_handler.broadcast(GamePhase.POT, PotArgs(pot=self.game.pot))
+
+        # big blind
+        self.game.bb_pos = (self.game.sb_pos + 1) % len(self.game.players)
+        self.game.players[self.game.bb_pos].balance -= self.game.bb_amount
+        self.game.players[self.game.bb_pos].bet = self.game.bb_amount
+        self.game.pot += self.game.bb_amount
+        await self.game.game_handler.broadcast(GamePhase.PRE_FLOP_BB, PreFlopBBArgs(
+            bb_amount=self.game.bb_amount,
+            player=self.game.players[self.game.bb_pos]))
+        await self.game.game_handler.broadcast(GamePhase.POT, PotArgs(pot=self.game.pot))
+
+    @override
+    def _change_turn_options(self, curr_player_pos, turn_options: List[PlayerChoice])->List[PlayerChoice]:
+        if curr_player_pos == self.game.bb_pos: return [PlayerChoice.CHECK, PlayerChoice.FOLD, PlayerChoice.RAISE]
+        else: return turn_options
+
+class FlopState(AbsGameState):
+    def _get_init_turn_options(self):
+        return [PlayerChoice.CHECK, PlayerChoice.FOLD, PlayerChoice.RAISE]
+
+    def _get_init_aggressor_pos(self):
+        return self.game.sb_pos
+
+    def _get_starting_pos(self):
+        return self.game.sb_pos
+
+    async def _before_betting_round_action(self):
+        for p in self.game.players: p.bet = 0
+
+    async def start_flow(self):
+        await self._deal_community_cards(3)
+        await self.run_betting_round()
